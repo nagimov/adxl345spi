41b3fc9532db07fc7fe891e863c87616ad02ba37
abliznyuk <bliznyuk.andrey@gmail.com>
2019-05-07 16:17:51 +0300

    fixes

--- writer.h
+++ writer.h
@@ -14,28 +14,29 @@ struct AccelData {
 
 class ADXLWriter {
  public:
+  virtual ~ADXLWriter() = 0;
   virtual void write(const AccelData& data) = 0;
 };
 
 class ConsoleADXLWriter : public ADXLWriter {
  public:
-  void write(const AccelData& data);
+  void write(const AccelData& data) override;
 };
 
 class FileADXLWriter : public ADXLWriter {
  public:
-  FileADXLWriter(const char* filename, int verbose);
-  ~FileADXLWriter();
-  void write(const AccelData& data);
+  FileADXLWriter(const char *filename, int verbose);
+  ~FileADXLWriter() override;
+  void write(const AccelData& data) override;
  private:
   FILE *f;
-  const char* filename;
+  const char *filename;
   int verbose{};
 };
 
 class RollupFileADXLWriter : public ADXLWriter {
  public:
-  void write(const AccelData& data);
+  void write(const AccelData& data) override;
 };
 
 ADXLWriter *createWriter(const params& cfg);
