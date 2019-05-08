2d46a2bb85aa134c4e3e5b97d8987e53d83dc5a9
abliznyuk <bliznyuk.andrey@gmail.com>
2019-05-08 10:32:17 +0300

    fixes

--- utils.cpp
+++ utils.cpp
@@ -3,6 +3,6 @@
 unsigned long long getTime()
 {
   struct timeval tv;
-  gettimeofday(&tv, NULL);
+  gettimeofday(&tv, nullptr);
   return (unsigned long long) (tv.tv_sec) * 1000 + (unsigned long long) (tv.tv_usec) / 1000;
 }
